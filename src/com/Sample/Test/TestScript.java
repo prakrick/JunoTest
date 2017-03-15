fcf03688160a637293db9ef75b088e51804066a4
prakrick <prakrick@gmail.com>
2017-03-15 17:34:05 +0530

    hi initial commit

---
+++ src/com/Sample/Test/TestScript.java
@@ -0,0 +1,14 @@
+package com.Sample.Test;
+
+public class TestScript {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+
+	}
+
+}
